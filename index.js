32d7f64d0d159aad50595976ceee19a0b58f5e91
andreschaparro <chaparroandres87@gmail.com>
2023-11-15 20:02:45 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000
+
+const datos = require("./datos.json");
+
+app.get("/devices", (req, res) => {
+    res.send(datos);
+});
+
+app.listen(port, () => {
+    console.log(`API funcionando en el puerto ${port}`)
+});
